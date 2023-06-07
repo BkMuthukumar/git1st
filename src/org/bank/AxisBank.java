2decd22f622154ebe6dc460a88fad52ef3f58f69
BKMuthukumar <1704bmk@gmail.com>
2023-06-07 16:47:23 +0530

    t4

--- src/org/bank/AxisBank.java
+++ src/org/bank/AxisBank.java
@@ -15,6 +15,7 @@ public void fixed() {
 public void deposit() {
 	System.out.println("AXIS BANK  DEPOSIT: 200000");
 	super.deposit();
+	System.out.println("sghdw");
 }
 // MAIN 
    public static void main(String[] args) {
