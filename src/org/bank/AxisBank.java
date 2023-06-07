e0bc3496660069fde1a45210140f383c5002e861
BKMuthukumar <1704bmk@gmail.com>
2023-06-07 10:31:31 +0530

    bank details

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,28 @@
+package org.bank;
+
+public class AxisBank extends BankInfo {
+	@Override
+	public void saving() {
+		System.out.println("AXIS BANK SAVING: 1000");
+		super.saving();
+	}
+@Override
+public void fixed() {
+	System.out.println("AXIS BANK FIXED:100000");
+	super.fixed();
+}
+@Override
+public void deposit() {
+	System.out.println("AXIS BANK  DEPOSIT: 200000");
+	super.deposit();
+}
+// MAIN 
+   public static void main(String[] args) {
+	//OBJECT
+	   AxisBank a =new AxisBank();
+	   // call method
+	   a.deposit();
+	   a.fixed();
+	   a.saving();
+}
+}
---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class BankInfo {
+	public void saving() {
+		System.out.println("SAVING = 20000");
+	}
+	public void fixed() {
+		System.out.println("FIXED =100000");
+	}
+	public void deposit() {
+		System.out.println("DEPOSIT =100000");
+	}
+	
+	
+}
